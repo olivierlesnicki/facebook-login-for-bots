5fadd18c749f7ffd71e8b41b04803126f0366c92
Olivier Lesnicki <olivier.lesnicki@gmail.com>
2016-08-31 22:27:32 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import queryString from 'query-string';
+import fetch from 'node-fetch';
+import loading from './loading.svg';
+import './App.css';
+
+const queryParams = queryString.parse(location.search);
+
+const APP_ID = queryParams.app_id;
+const SCOPE = queryParams.scope || 'public_profile';
+const ACCOUNT_LINKING_TOKEN = queryParams.account_linking_token;
+const AUTHORIZATION_CODE_URI = queryParams.authorization_code_uri;
+const REDIRECT_URI = queryParams.redirect_uri;
+
+class App extends Component {
+
+  constructor() {
+    super(...arguments);
+
+    this._loginFailure = this._loginFailure.bind(this);
+    this._loginSuccess = this._loginSuccess.bind(this);
+  }
+
+  _loginFailure() {
+    window.location = `${REDIRECT_URI}?account_linking_token=${ACCOUNT_LINKING_TOKEN}`;
+  }
+
+  _loginSuccess(authorizationCode) {
+    window.location = `${REDIRECT_URI}?account_linking_token=${ACCOUNT_LINKING_TOKEN}&authorization_code=${authorizationCode}`;
+  }
+
+  componentDidMount() {
+    window.fbAsyncInit = function() {
+      let FB = window.FB;
+
+      FB.init({
+        appId      : APP_ID,
+        cookie     : true,
+        version    : 'v2.5'
+      });
+
+      FB.login((response) => {
+        if (response.authResponse) {
+          fetch(AUTHORIZATION_CODE_URI, {
+            method: 'POST',
+            body: JSON.stringify({
+              accountLinkingToken: ACCOUNT_LINKING_TOKEN,
+              authResponse
+            })
+          })
+            .then(res => res.json())
+            .then(json => {
+              this._loginSuccess(json.authorizationCode);
+            });
+        } else {
+          this._loginFailure();
+        }
+      }, {scope: SCOPE});
+    };
+
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <img
+          className="App-loading"
+          alt="Loading"
+          src={loading}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
